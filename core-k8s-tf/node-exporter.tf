29849f3c7e80f3e28bc93761d6ac195115731d0d
nthskyradiated <andy.pandaan@outlook.com>
2025-01-10 12:24:50 +0400

    working on noe-exporter tf

--- core-k8s-tf/node-exporter.tf
+++ core-k8s-tf/node-exporter.tf
@@ -6,4 +6,39 @@ resource "helm_release" "prometheus_node_exporter" {
   namespace        = "monitoring"
   create_namespace = true
   version          = "4.43.1"
+
+  set {
+    name  = "service.portName"
+    value = "node-exporter"
+  }
+  set {
+    name  = "service.annotations.prometheus.io/scrape"
+    value = true
+  }
+  set {
+    name  = "service.annotations.prometheus.io/port"
+    value = "9100"
+  }
+  set {
+    name = "prometheus.monitor.enabled"
+    value = true
+  }
+  set {
+    name = "prometheus.monitor.namespace"
+    value = "monitoring"
+  }
+  set {
+    name = "podMonitor.enabled"
+    value = true
+  }
+  set {
+    name = "podMonitor.namespace"
+    value = "monitoring"
+  }
+  set {
+    name = "podMonitor.additionalLabels"
+    value = {
+      "prometheus" = "main"
+    }
+  }
 }
