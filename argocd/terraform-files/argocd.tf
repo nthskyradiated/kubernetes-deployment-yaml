a73a0df9762a3aa496f175857522ce65070daaf5
nthskyradiated <andy.pandaan@outlook.com>
2025-01-16 07:58:59 +0100

    updated argocd's tf template

--- argocd/terraform-files/argocd.tf
+++ argocd/terraform-files/argocd.tf
@@ -5,7 +5,7 @@ resource "helm_release" "argocd" {
   chart            = "argo-cd"
   namespace        = "argocd"
   create_namespace = true
-  version          = "7.6.12"
+  version          = "7.7.15"
 
   values = [file("values/argocd.yaml")]
 }
